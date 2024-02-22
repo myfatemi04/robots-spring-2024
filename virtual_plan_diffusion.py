06525dcaa478ab0a8b49c0935a3c1f056abdf481
Michael Fatemi <myfatemi04@gmail.com>
2024-02-22 13:41:43 -0500

    loss function runs

--- virtual_plan_diffusion.py
+++ virtual_plan_diffusion.py
@@ -15,13 +15,19 @@ Experiment 3: Video Infilling as Hierarchical Planning
 """
 
 import json
+from typing import List
 
+import numpy as np
+import PIL
+import PIL.Image
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import transformers
+from transformers.models.clip.modeling_clip import \
+    BaseModelOutput as CLIPBaseModelOutput
 from transformers.tokenization_utils_base import BatchEncoding
-from transformers.models.clip.modeling_clip import BaseModelOutput as CLIPBaseModelOutput
+
 
 class NoiseConditionedScoreModel(nn.Module):
   def __init__(self,
@@ -41,7 +47,7 @@ class NoiseConditionedScoreModel(nn.Module):
     self.diffusion_alpha_cumprods = torch.cumprod(1 - diffusion_betas, dim=0)
     self.temporal_embeddings = nn.Embedding(max_trajectory_length, d_model)
 
-    TRAJECTORY_DIMS = 6
+    TRAJECTORY_DIMS = 7
     self.embed_trajectory = nn.Sequential(
       nn.Linear(TRAJECTORY_DIMS, d_model),
       nn.ReLU(),
@@ -49,7 +55,7 @@ class NoiseConditionedScoreModel(nn.Module):
     )
     self.unembed_trajectory_noise = nn.Linear(d_model, TRAJECTORY_DIMS)
 
-    self.downsample_clip_tokens = nn.Linear(512, d_model) if self.d_model != 512 else nn.Identity()
+    self.downsample_clip_tokens = nn.Linear(768, d_model) if self.d_model != 768 else nn.Identity()
 
     num_diffusion_steps = len(diffusion_betas)
     self.noise_conditioning_embeddings = nn.Embedding(num_diffusion_steps, d_model)
@@ -64,7 +70,7 @@ class NoiseConditionedScoreModel(nn.Module):
     )
   
   def predict_noise(self,
-                    cue: torch.Tensor,
+                    cue: List[PIL.Image.Image],
                     trajectory: torch.Tensor,
                     diffusion_step: torch.Tensor) -> torch.Tensor:
     """
@@ -76,7 +82,7 @@ class NoiseConditionedScoreModel(nn.Module):
      - `trajectory`: [batch, timesteps, TRAJECTORY_DIMS]
     """
     # 1.) Encode the cue image.
-    cue_clip_input: BatchEncoding = self.clip_processor(images=[cue], return_tensors="pt")
+    cue_clip_input: BatchEncoding = self.clip_processor(images=cue, return_tensors="pt")
     cue_clip_output: CLIPBaseModelOutput = self.clip(**cue_clip_input.data)
     cue_clip_tokens_direct = cue_clip_output.last_hidden_state
     num_cue_images, num_clip_tokens, clip_token_dimension = cue_clip_tokens_direct.shape
@@ -84,12 +90,12 @@ class NoiseConditionedScoreModel(nn.Module):
 
     num_trajectories, trajectory_length, _trajectory_dims = trajectory.shape
 
-    if cue.dim() == 3:
+    if len(cue) == 1:
       # No batch dimension for clip.
       # Must repeat the cue tokens.
       # This is useful for training: We can denoise several
       # trajectories for the same input image at once.
-      cue_clip_tokens = cue_clip_tokens.unsqueeze(0).repeat((num_trajectories, 1, 1))
+      cue_clip_tokens = cue_clip_tokens.repeat((num_trajectories, 1, 1))
     else:
       assert num_cue_images == num_trajectories, "Number of cue images must match number of trajectories, or there must be exactly one cue image provided."
 
@@ -104,7 +110,7 @@ class NoiseConditionedScoreModel(nn.Module):
     input_tokens = torch.cat([
       cue_clip_tokens,
       trajectory_embedding,
-      noise_conditioning_embedding
+      noise_conditioning_embedding.unsqueeze(-2)
     ], dim=-2)
     embedded_tokens = self.score_function_backbone(input_tokens)
 
@@ -114,7 +120,7 @@ class NoiseConditionedScoreModel(nn.Module):
 
     return trajectory_noise
 
-  def loss(self, cue: torch.Tensor, trajectory: torch.Tensor):
+  def loss(self, cue: List[PIL.Image.Image], trajectory: torch.Tensor):
     """
     Assumes the trajectory is normalized to [-1, 1].
     """
@@ -128,10 +134,15 @@ class NoiseConditionedScoreModel(nn.Module):
     # Now, we add the noise. This is done with the reparametrization trick
     # that allows us to sample at arbitrary diffusion steps.
     # 1. We scale each value by sqrt{^alpha_t}.
-    noised_trajectories *= torch.sqrt(self.diffusion_alpha_cumprods)[(None,) * trajectory.dim()]
+    sqrt_alphas = torch.sqrt(self.diffusion_alpha_cumprods)
+    sqrt_invalphas = torch.sqrt(1 - self.diffusion_alpha_cumprods)
+    for i in range(trajectory.dim()):
+      sqrt_alphas.unsqueeze_(-1)
+      sqrt_invalphas.unsqueeze_(-1)
+    noised_trajectories *= sqrt_alphas
     # 2. We add noise, with a variance of $1 - \overbar{\alpha}_t$.
     noise = torch.normal(0, 1, size=noised_trajectories.shape)
-    noise *= torch.sqrt(1 - self.diffusion_alpha_cumprods)[(None,) * trajectory.dim()]
+    noise *= sqrt_invalphas
     noised_trajectories += noise
 
     # Now, we calculate the loss: MSE over all timesteps.
@@ -150,12 +161,55 @@ class NoiseConditionedScoreModel(nn.Module):
 
     return loss
 
-# Starting with a 3D trajectory and camera setup, we gradually add noise to the points.
-
 input_folder = "control_labels/trajectories/003_grab_water_bottle_by_side"
 with open(input_folder + "/data.json", "r") as f:
   trajectory = json.load(f)
 
-# dict_keys(['times', 'poses', 'quaternions', 'gripper_widths', 'gripper_fully_closed_width', 'gripper_fully_open_width'])
-# print(trajectory.keys())
+"""
+>>> print(trajectory.keys())
 
+dict_keys([
+  'times',
+  'poses',
+  'quaternions',
+  'gripper_widths',
+  'gripper_fully_closed_width',
+  'gripper_fully_open_width'
+])
+"""
+
+image = PIL.Image.open("control_labels/instructions/003_grab_water_bottle_by_side_start_position_left.png")
+image_array = np.array(image)
+
+# For now, let's ignore quaternions... those are annoying :(
+# Instead, let's just predict position.
+# We'll also ignore gripper width for now.
+
+clip_vision_model: transformers.CLIPVisionModel = transformers.CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32") # type: ignore
+clip_processor = transformers.CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+model = NoiseConditionedScoreModel(
+  clip_vision_model,
+  clip_processor,
+  diffusion_betas=torch.linspace(0.001, 0.01, 100),
+  max_trajectory_length=32,
+  d_model=64,
+  num_backbone_heads=4,
+  num_backbone_layers=4
+)
+
+# Put trajectory into a tensor.
+# {x, y, z}: [0, 1, 2]
+# {euler angles [3]}: [3, 4, 5]
+# {gripper state}: [6]
+trajectory_tensor = torch.zeros((len(trajectory["poses"]), 7))
+trajectory_tensor[:, [0, 1, 2]] = torch.tensor(trajectory["poses"])
+trajectory_tensor[:, 6] = torch.tensor(trajectory["gripper_widths"])
+
+optim = torch.optim.Adam(model.parameters(), lr=1e-3)
+for i in range(100):
+  optim.zero_grad()
+  loss = model.loss([image], trajectory_tensor[:32])
+  loss.backward()
+  optim.step()
+  print(loss.item())
