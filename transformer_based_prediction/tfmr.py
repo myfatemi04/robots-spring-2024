f01504ea23a0f77d6a520dc12311a2c5fc5c0b7b
Michael Fatemi <myfatemi04@gmail.com>
2024-02-29 18:52:22 -0500

    add spatiotemporal transformer

--- transformer_based_prediction/tfmr.py
+++ transformer_based_prediction/tfmr.py
@@ -27,6 +27,8 @@ class SpatioTemporalDetokenizer(nn.Module):
 
 class SpatioTemporalEmbedding(nn.Module):
     def __init__(self, n_latent_dims: int, max_width: int, max_height: int, max_temporal_tokens: int):
+        super().__init__()
+
         self.n_latent_dims = n_latent_dims
 
         # These will be taken in row-major order
@@ -60,15 +62,80 @@ class SpatioTemporalEmbedding(nn.Module):
         return x
 
 class SpatioTemporalAttentionBlock(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, n_latent_dims: int, n_spatial_attention_heads: int, n_temporal_attention_heads: int, n_mlp_dims: int):
+        super().__init__()
+        
+        self.n_latent_dims = n_latent_dims
+        self.n_spatial_attention_heads = n_spatial_attention_heads
+        self.n_temporal_attention_heads = n_temporal_attention_heads
+
+        self.spatial_attention = nn.MultiheadAttention(n_latent_dims, n_spatial_attention_heads, batch_first=True)
+        self.temporal_attention = nn.MultiheadAttention(n_latent_dims, n_temporal_attention_heads, batch_first=True)
+        self.mlp = nn.Sequential(
+            nn.Linear(n_latent_dims, n_mlp_dims),
+            nn.ReLU(),
+            nn.Linear(n_mlp_dims, n_latent_dims)
+        )
+
+    def forward(self, x):
+        # x: [batch_size, seqlen, height, width, n_latent_dims]
+        (batch_size, seqlen, height, width, n_latent_dims) = x.shape
+
+
+        # Spatial attention
+        # Make seqlen a batch dimension by reshaping to [batch_size * seqlen, height * width, n_latent_dims]
+        x = x.view(batch_size * seqlen, height * width, n_latent_dims)
+        x, _ = self.spatial_attention(x, x, x)
+        x = x.view(batch_size, seqlen, height, width, n_latent_dims)
+
+        # Temporal attention
+        # Permute to [batch_size, height, width, seqlen, n_latent_dims]
+        x = x.permute(0, 2, 3, 1, 4)
+        # Make height * width a batch dimension by reshaping to [batch_size * height * width, seqlen, n_latent_dims]
+        x = x.view(batch_size * height * width, seqlen, n_latent_dims)
+        x, _ = self.temporal_attention(x, x, x)
+        x = x.view(batch_size, height, width, seqlen, n_latent_dims)
+        # Permute back to [batch_size, seqlen, height, width, n_latent_dims]
+        x = x.permute(0, 3, 1, 2, 4)
+
+        # Linear residual layer
+        x = x + self.mlp(x)
+
+        return x
 
 class SpatioTemporalTransformer(nn.Module):
-    def __init__(self, token_size: int, n_latent_dims: int, n_image_dims: int):
+    def __init__(self,
+                 token_size: int,
+                 n_latent_dims: int,
+                 n_image_dims: int,
+                 max_width: int,
+                 max_height: int,
+                 max_temporal_tokens: int,
+                 n_spatial_attention_heads: int,
+                 n_temporal_attention_heads: int,
+                 n_mlp_dims: int,
+                 n_layers: int,
+                ):
         super().__init__()
 
         self.tokenizer = SpatioTemporalTokenizer(token_size, n_latent_dims, n_image_dims)
+        self.embedder = SpatioTemporalEmbedding(n_latent_dims, max_width, max_height, max_temporal_tokens)
         self.detokenizer = SpatioTemporalDetokenizer(token_size, n_latent_dims, n_image_dims)
 
+        self.attention_blocks = nn.ModuleList([
+            SpatioTemporalAttentionBlock(
+                n_latent_dims,
+                n_spatial_attention_heads,
+                n_temporal_attention_heads,
+                n_mlp_dims
+            )
+            for _ in range(n_layers)
+        ])
+
     def forward(self, x):
-        pass
+        x = self.tokenizer(x)
+        x = self.embedder(x)
+        for attention_block in self.attention_blocks:
+            x = attention_block(x)
+        x = self.detokenizer(x)
+        return x
