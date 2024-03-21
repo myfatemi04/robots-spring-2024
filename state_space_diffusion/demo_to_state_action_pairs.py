fix some incorrect quaternions; test using arbitrary extrinsic camera matrices

state_space_diffusion/demo_to_state_action_pairs.py
@@ -84,7 +84,6 @@ def create_torch_dataset(demos, device):
 def create_orthographic_labels_v2(demo: Demo, renderer: VoxelRenderer, device="cuda", include_extra_metadata=False):
     keypoints = get_keypoint_observation_indexes(demo)
 
-
     assert keypoints[0] != 0, "Start position is not a keypoint."
 
     previous_pos = 0
@@ -96,7 +95,10 @@ def create_orthographic_labels_v2(demo: Demo, renderer: VoxelRenderer, device="c
         start_obs = demo[previous_pos]
         target_obs = demo[keypoint]
         target_pos = target_obs.gripper_pose[:3]
-        target_quat = target_obs.gripper_pose[3:]
+        # We use this quaternion, as it is in world frame.
+        target_quat = Q.rotation_matrix_to_quaternion(target_obs.gripper_matrix[:3, :3])
+        # This one has messed up axes for some reason. Maybe it is from the perspective of some other frame.
+        # target_quat = target_obs.gripper_pose[3:]
 
         # Get point clouds for virtual views.
         pcds = [torch.tensor(getattr(start_obs, camera + '_point_cloud').reshape(-1, 3)) for camera in cameras]

state_space_diffusion/quaternions.py
@@ -24,17 +24,22 @@ ROTATE_CAMERA_QUATERNION_TO_WORLD_QUATERNION = {
     # what is in camera x/y is actually in world x/z.
     # thus we rotate around camera x by -np.pi/2
     'xz': compose_quaternions(
-        create_quaternion(np.pi/2, [0, 1, 0]),
-        create_quaternion(np.pi, [0, 0, 1]),
+        create_quaternion(-np.pi/2, [1, 0, 0]),
+        create_quaternion(0, [0, 0, 1]),
+        # create_quaternion(np.pi, [0, 0, 1]),
     ),
     # what is in camera x/y is actually in world y/z.
     # thus we rotate camera z up to camera y first (bringin it to world z),
     # then we rotate camera x to world z
     'yz': compose_quaternions(
-        create_quaternion(-np.pi/2, [0, 0, 1]), # applied first
-        create_quaternion(-np.pi/2, [1, 0, 0]), # applied second
+        create_quaternion(-np.pi/2, [0, 0, 1]), # applied second
+        create_quaternion(-np.pi/2, [1, 0, 0]), # applied first
     )
 }
+
+"""
+
+"""
 
 ROTATE_WORLD_QUATERNION_TO_CAMERA_QUATERNION = {
     # Inverse of a quaternion can be found by negating sin(theta/2).
@@ -46,7 +51,9 @@ def rotation_matrix_to_quaternion(matrix: np.ndarray):
         matrix = matrix[:3, :3]
     else:
         assert matrix.shape == (3, 3), f"Matrix shape is {matrix.shape}. Must be 3x3 (R) or 4x4 (RT)."
-    return Rotation.from_matrix(matrix).as_quat()
+    quat = Rotation.from_matrix(matrix).as_quat()
+    # scipy uses scalar-last format
+    return np.array([quat[-1], *quat[:-1]])
 
 def quaternion_to_rotation_matrix(quat: np.ndarray):
     # scipy uses scalar-last format

state_space_diffusion/test_v8.py
@@ -1,4 +1,4 @@
-### Goal: Make something that can effectively denoise towards the point (2/3, 2/3).
+### v8: 
 
 from functools import partial
 
@@ -74,7 +74,7 @@ def scaled_arrows(image, pixel_scaled_positions, pred_direction, true_direction)
         label='True'
     )
 
-def train(demos, epochs=20):
+def train():
     device = torch.device('cuda')
 
     clip = transformers.CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch16").to(device) # type: ignore
@@ -95,12 +95,12 @@ def train(demos, epochs=20):
 
     optim = torch.optim.Adam([p for p in model.parameters() if p.requires_grad], lr=1e-5)
 
-    dataset = create_torch_dataset_v2(demos, device=device)
+    dataset = create_torch_dataset_v2(get_demos(), device=device)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=16, shuffle=True)
 
     grid_size = 14
 
-    for epoch in range(epochs):
+    for epoch in range(20):
         for (image, position, quat) in (pbar := tqdm.tqdm(dataloader)):
             pixel_values = clip_processor(images=image, return_tensors="pt", do_rescale=False).to(device=device).pixel_values # type: ignore
 
@@ -212,7 +212,7 @@ def sample(model, renderer, xy_image, yz_image, xz_image, start_point, device="c
 
     xmin, ymin, zmin, xmax, ymax, zmax = SCENE_BOUNDS
 
-    n_steps = 50
+    n_steps = 10
     step_sizes = torch.linspace(0.2, 0.1, n_steps, device=device)
     grid_size = 14
     grid_square_size = 16
@@ -263,7 +263,7 @@ def sample(model, renderer, xy_image, yz_image, xz_image, start_point, device="c
 
     return history, history_quats, (history_xy, history_yz, history_xz), (score_xy_map, score_yz_map, score_xz_map)
 
-def evaluate(demo, output_prefix):
+def evaluate():
     device = torch.device("cuda")
     clip = transformers.CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch16")
     model = VisualPlanDiffuserV7(clip).to(device) # type: ignore
@@ -271,7 +271,8 @@ def evaluate(demo, output_prefix):
 
     renderer = VoxelRenderer(SCENE_BOUNDS, 224, torch.tensor([0, 0, 0], device=device), device=device)
 
-    state_action_tuples = create_orthographic_labels(demo, renderer, device, include_extra_metadata=True) # type: ignore
+    demos = get_demos()
+    state_action_tuples = create_orthographic_labels(demos[0], renderer, device, include_extra_metadata=True) # type: ignore
 
     # Test with first keypoint.
     (yz_image, xz_image, xy_image), (yz_pos, xz_pos, xy_pos), (start_obs, target_obs) = state_action_tuples[0]
@@ -288,7 +289,7 @@ def evaluate(demo, output_prefix):
     ax.set_ylim(-viewbox_size, viewbox_size)
     ax.set_zlim(-viewbox_size, viewbox_size)
     ax.plot([point[0].item() for point in history], [point[1].item() for point in history], [point[2].item() for point in history])
-    plt.savefig(output_prefix + "_3d_sampling_trajectory.png")
+    plt.savefig("3d_sampling_trajectory.png")
     # plt.show()
 
     tensor2numpy = lambda x: x.detach().cpu().numpy()
@@ -363,10 +364,7 @@ def evaluate(demo, output_prefix):
 
 
     plt.tight_layout()
-    plt.savefig(output_prefix + "_2d_multiview_sampling_trajectory.png", dpi=512)
+    plt.savefig("2d_multiview_sampling_trajectory.png", dpi=512)
 
-demos = get_demos()
-print("Running training. [10 epochs]")
-train(demos[:7], epochs=10)
-print("Running evaluation on held-out demo.")
-evaluate(demos[-1], 'demo_7')
+# train()
+evaluate()