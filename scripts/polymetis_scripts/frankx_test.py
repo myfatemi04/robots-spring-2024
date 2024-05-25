68b767b2ea8339b3d881f4a13ede4d402c646d9e
Michael Fatemi <myfatemi04@gmail.com>
2024-05-25 13:36:21 -0400

    Update frankx_test.py

--- scripts/polymetis_scripts/frankx_test.py
+++ scripts/polymetis_scripts/frankx_test.py
@@ -3,7 +3,47 @@ import numpy as np
 from scipy.spatial.transform import Rotation
 import time
 
-gp = frankx.Gripper('192.168.1.162')
+rb = frankx.Robot("192.168.1.163")
+state = rb.get_state()
+print(dir(state))
+print(np.array(state.O_T_EE).reshape(4, 4).T)
+print(np.array(state.O_T_EE_c).reshape(4, 4).T)
+print(np.array(state.O_T_EE_d).reshape(4, 4).T)
+print(np.array(state.F_T_EE).reshape(4, 4).T)
+# print(np.array(state.F_T_NE).reshape(4, 4).T)
+
+
+ang = np.pi
+
+target_matrix = np.array([
+    [1, 0, 0],
+    [0, np.cos(ang), -np.sin(ang)],
+    [0, np.sin(ang), np.cos(ang)],
+])
+
+# target_matrix = np.array([
+#     [0, 1, 0],
+#     [1, 0, 0],
+#     [0, 0, -1],
+# ])
+
+print(target_matrix)
+
+rot = Rotation.from_matrix(target_matrix)
+qx, qy, qz, qw = rot.as_quat()
+
+# print(qx, qy, qz, qw)
+
+# for _ in range(3):
+rb.recover_from_errors()
+rb.set_dynamic_rel(0.1)
+rb.move(frankx.LinearMotion(frankx.Affine(0.4, 0.0, 0.4, qw, qx, qy, qz)))
+
+# input()
+
+exit()
+
+gp = frankx.Gripper('192.168.1.163')
 gp.move(0.08)
 print(gp.max_width)
 print(gp.has_error)
@@ -44,4 +84,3 @@ robot.move(frankx.LinearMotion(frankx.Affine(0.6, 0.0, 0.4, qw, qx, qy, qz)))
 # time.sleep(1)
 
 # gripper.release()
-
